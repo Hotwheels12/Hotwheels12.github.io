e2a9889dd2c841d85a6603ee432c58130c25a639
Hotwheels <102762018+Hotwheels12@users.noreply.github.com>
2022-05-16 08:50:09 -0500

    Add files via upload

---
+++ php.php
@@ -0,0 +1,11 @@
+<?php
+	$n=$_post["n"];
+	$a=$_post["a"];
+	$d=$_post["d"];
+	$c=$_post["c"];
+	$u=$_post["u"];
+
+	echo"Bienvenido" .$n. " " .$a. " a Hotwheels <br>";
+
+	echo"Te enviaremos un email al correo: " .$c. " gracias por ser parte de nuestra familia Hotwheels";
+?>
